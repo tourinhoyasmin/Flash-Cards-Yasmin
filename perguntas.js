71b0be685fd80cc4ec11cff332c9439a550c107d
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2024-10-24 14:56:15 -0300

    24/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Cinema',
+    'Quantos filmes da saga Velozes e Furiosos foram feitos?',
+    '11 filmes'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Cinema',
+    'Em qual ano foi lançado a série The Vampire Diaries?',
+    'No ano de 2009'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Cinema',
+    'Quantas temporadas tem na série Game of Thrones?',
+    ' 8 temporadas'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Cinema',
+    'Qual filme atual que fala sobre as principais emoções do ser humano?',
+    'Divertida mente 2'
 )
