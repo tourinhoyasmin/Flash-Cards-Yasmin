0a5641c7cdc63a43a83b476a01f797dff1f186cb
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2024-11-14 14:54:28 -0300

    14/11

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function criaCartao(categoria, pergunta, resposta){
                 </div>
                 `
 
-        let respostaEstaVisivel = false
+        let respostaEstaVisivel = false;
 
         function viraCartao(){
             respostaEstaVisivel = !respostaEstaVisivel;
