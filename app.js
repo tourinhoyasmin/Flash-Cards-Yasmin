651b34e1465d4cd9f72939e795ac1bf7de778938
tourinhoyasmin <145395964+tourinhoyasmin@users.noreply.github.com>
2024-11-14 14:14:55 -0300

    14/14

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ function criaCartao(categoria, pergunta, resposta){
                     </div>
                 </div>
                 `
+
+        let respostaEstaVisivel = false
+
+        function viraCartao(){
+            respostaEstaVisivel = !respostaEstaVisivel;
+            cartao.classList.toggle('active', respostaEstaVisivel);
+        }
+
+        cartao.addEventListener('click', viraCartao);
+
+        container.appendChild(cartao)
+
+
     container.appendChild(cartao);            
 
 }
